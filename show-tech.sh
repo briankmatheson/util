4f254bd3f2089cb70dfece9d333d51373bfa73e4
Brian Matheson <bmath@wit.com>
2019-10-02 16:53:33 -0400

    initial checkin of show-tech.sh

---
+++ show-tech.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+touch /tmp/maya-tech-support
+
+while read line; do
+		$line >> /tmp/maya-tech-support
+		done <<EOF
+kubectl version
+kubectl get nodes
+kubectl get bd -A
+kubectl get spc
+kubectl get sp
+kubectl get pvc -A
+kubectl get pv
+kubectl cluster-info dump -A
+EOF
+
+cat /tmp/maya-tech-support | gzip -c > ./maya-tech-support.gz
