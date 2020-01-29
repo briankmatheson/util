be6d7acfa72f86dd869066522d03614a198f02ff
Brian Matheson <bmath@wit.com>
2020-01-29 05:28:16 -0500

    .

--- show-bds.sh
+++ show-bds.sh
@@ -3,5 +3,7 @@
 kubectl get bd -n openebs -o custom-columns=NAME:spec.claimRef.name,\
 NODE:spec.nodeAttributes.nodeName,\
 PATH:spec.path,\
+SIZE:spec.capacity.storage,\
+MODEL:spec.details.model,\
 CLAIM:status.claimState,\
 STATE:status.state
