065fa419a310516037c5f824e2c3b6c074c011e5
Brian Matheson <bmath@wit.com>
2020-05-27 07:32:42 -0400

    .

--- show-openebs-nodes.sh
+++ show-openebs-nodes.sh
@@ -2,32 +2,25 @@
 
 infra_hosts=`kubectl --no-headers=true get pod -n openebs -owide | awk '{print $7}' | sort -u`
 echo OpenEBS Enterprise Infrastructure hosts:
-echo $infra_hosts
+for h in $infra_hosts; do
+		echo $h
+done
+
 echo OpenEBS Enterprise Infrastructure consumers:
 
-kubectl --no-headers=true get pvc -A -o 'custom-columns=NAMESPACE:metadata.namespace,NAME:metadata.name,ANNOTATIONS:metadata.annotations' | tr ']' ' ' | while read namespace name rest
+kubectl --no-headers=true get pv -A -o 'custom-columns=NAME:metadata.name,ANNOTATIONS:metadata.annotations' | tr ']' ' ' | while read name rest
 do
 		for word in $rest
 		do
 				case "$word" in
-						 *kubernetes.io/storage-provisioner:*)
-								 provisioner=`echo $word | tr ':' ' ' | awk '{print $2}'`
-								 
-																									;;
-						 *kubernetes.io/selected-node:*)
-								 node=`echo $word | tr ':' ' ' | awk '{print $2}'`
-								 
-																									;;
+						openebs.io/cas-type:cstor)
+								echo $rest | sed -E 's/^.+openEBSProvisionerIdentity:(\w+) .+$/\1/'
+								 ;;
   			esac
 		done
-		case "$provisioner" in
-				*openebs.io*)
-						client_count=`expr $client_count + 1`
-						echo $node
-						;;
-		esac
-done >3
-consumers=`sort -u <3`
+done >>/tmp/nodes.$$.tmp
+consumers=`sort -u </tmp/nodes.$$.tmp`
+rm -r /tmp/nodes.$$.tmp
 echo $consumers
 all_hosts=`echo $consumers $infra_hosts | tr ' ' '\n' | sort -u`
 count=`echo $all_hosts | wc -w`
